e67c0702d0a34080ae4a08d2c5701d6fee2ffef9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-03 17:10:20 +0200

    React welcome

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
 'use strict'
+
+//JSX - спеціальний синтаксис (синтаксичний цукор) 
+
+//Babel: jsx => js
+
+const root = ReactDOM.createRoot(document.getElementById('root')) 
+root.render(<App/>)
+
+function App (){
+    return <div titl='Hello!'>Hello React!</div>
+}
