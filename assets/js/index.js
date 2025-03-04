8c5bab805411bfb1aba28a5f309c2d70c5690e95
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-04 10:49:43 +0200

    Add Вітання з User

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,9 +10,12 @@ root.render(<App />);
 //<></> - react fragment для компонування кількох елементів
 function App() {
   const isLight = true;
+  const name = 'Nataliia'
   return (
     <>
       <ThemeButton isLightTheme={isLight} />
+      <Greeting isGreeting = {true} userName = {name}/>
+      <Greeting isGreeting = {false} userName = 'Roland'/>
     </>
   );
 }
@@ -26,3 +29,14 @@ function ThemeButton(props) {
   };
   return <button style={buttonStyle}>Switch to {isLightTheme ? "dark" : "light"}</button>;
 }
+
+function Greeting(props){
+  const {userName,isGreeting} = props
+  return <div> {isGreeting ? `Hello ${userName}`: `Goodbay ${userName}`}</div>
+}
+//----------------------------------------------------------------
+/*
+Реалізувати компонент isGreating, який буде виводити дані, який буде виводити рядок "Hello, ім'я",
+де ім'я задане в App
+
+*/
